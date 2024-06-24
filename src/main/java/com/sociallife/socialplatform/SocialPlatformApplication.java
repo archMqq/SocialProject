2e362f582dc5f9a9059e9646cf0df97fe19e9b0f
aanswerx <clouztoyou@mail.ru>
2024-06-24 18:18:16 +0300

    Создание HomeController и home.html, но есть ошибка запуска (вроде)

---
+++ src/main/java/com/sociallife/socialplatform/SocialPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sociallife.socialplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sociallife/socialplatform/SocialPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sociallife.socialplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
